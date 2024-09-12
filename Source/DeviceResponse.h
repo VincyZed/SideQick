2fbfaaba00c6406805f28ba8ed3b89987f3b9862
Vincent Zauhar <v.zauhar@bell.net>
2024-09-11 22:46:33 -0400

    Added file headers

--- Source/DeviceResponse.h
+++ Source/DeviceResponse.h
@@ -1,3 +1,17 @@
+/*
+ * SideQick - SQ-80/ESQ-1 expansion software
+ * Copyright Vincent Zauhar, 2024
+ *
+ * Released under the GNU General Public Licence v3
+ * or later (GPL-3.0-or-later). The license is found in the "LICENSE"
+ * file in the root of this repository, or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ *
+ * All source for SideQick is available at
+ * https://github.com/VincyZed/SideQick
+ */
+
 #pragma once
 #include <JuceHeader.h>
 
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,10 +1,16 @@
 /*
-  ==============================================================================
-
-    This file contains the basic startup code for a JUCE application.
-
-  ==============================================================================
-*/
+ * SideQick - SQ-80/ESQ-1 expansion software
+ * Copyright Vincent Zauhar, 2024
+ *
+ * Released under the GNU General Public Licence v3
+ * or later (GPL-3.0-or-later). The license is found in the "LICENSE"
+ * file in the root of this repository, or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ *
+ * All source for SideQick is available at
+ * https://github.com/VincyZed/SideQick
+ */
 
 #include <JuceHeader.h>
 #include "MainComponent.h"
@@ -23,31 +29,22 @@ public:
     //==============================================================================
     void initialise (const juce::String& commandLine) override
     {
-        // This method is where you should put your application's initialisation code..
-
         mainWindow.reset (new MainWindow (getApplicationName()));
     }
 
     void shutdown() override
     {
-        // Add your application's shutdown code here..
-
         mainWindow = nullptr; // (deletes our window)
     }
 
     //==============================================================================
     void systemRequestedQuit() override
     {
-        // This is called when the app is being asked to quit: you can ignore this
-        // request and let the app carry on running, or call quit() to allow the app to close.
         quit();
     }
 
     void anotherInstanceStarted (const juce::String& commandLine) override
     {
-        // When another instance of the app is launched while this one is running,
-        // this method is invoked, and the commandLine parameter tells you what
-        // the other instance's command-line arguments were.
     }
 
     //==============================================================================
@@ -79,18 +76,8 @@ public:
 
         void closeButtonPressed() override
         {
-            // This is called when the user tries to close this window. Here, we'll just
-            // ask the app to quit when this happens, but you can change this to do
-            // whatever you need.
             JUCEApplication::getInstance()->systemRequestedQuit();
         }
-
-        /* Note: Be careful if you override any DocumentWindow methods - the base
-           class uses a lot of them, so by overriding you might break its functionality.
-           It's best to do all your work in your content component instead, but if
-           you really have to override any DocumentWindow methods, make sure your
-           subclass also calls the superclass's method.
-        */
 
     private:
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
