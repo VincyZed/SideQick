6b1000df8a53682b64d4a86bba9efb1c5d1e1b52
Vincent Zauhar <50145178+VincyZed@users.noreply.github.com>
2025-01-18 23:30:56 -0500

    Beta 2.1

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -285,7 +285,7 @@ void MainComponent::showContextMenu() {
     menu.showMenuAsync(PopupMenu::Options(), [this](int result) {
         if (result == 2) {
             AlertWindow::showMessageBoxAsync(AlertWindow::NoIcon, "SideQick",
-                                             "Ensoniq SQ-80/ESQ-1 Expansion Software\nVersion Beta 2.0\n\nCopyright Vincent Zauhar, 2025\nReleased under the "
+                                             "Ensoniq SQ-80/ESQ-1 Expansion Software\nVersion Beta 2.1\n\nCopyright Vincent Zauhar, 2025\nReleased under the "
                                              "GNU GPL v3 license\n\nhttps://github.com/VincyZed/SideQick");
         } else if (result == 3)
             JUCEApplication::getInstance()->systemRequestedQuit();
